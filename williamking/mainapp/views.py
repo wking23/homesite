e1d799d72ae0645d1c9de538cf7d422d5666b9ba
William King <wdk23@cs.drexel.edu>
2016-05-09 15:32:01 -0400

    added twitter, resumé, but man it needs to be made pretty

--- williamking/mainapp/views.py
+++ williamking/mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import get_template
 # Create your views here.
 def index(request):
-    return HttpResponse("Howdy")
+    return render(request, 'index_template.html', {'title': 'William King'})
